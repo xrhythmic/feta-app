705e7ce379d0253890866797dc73580b78b40714
Jordan <56432409+xrhythmic@users.noreply.github.com>
2021-11-04 19:21:11 +0000

    Add Basic Front End for Posts CW3

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<x-post-layout>
+    @foreach ($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->id }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+
+            <p>
+                By <a href="/authors/{{ $post->author->username }}">
+                    {{ $post->author->name }}
+                </a> in <a href="/categories/{{ $post->category->slug }}">
+                    {{ $post->category->name }}
+                </a>
+            </p>
+
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+</x-post_layout>
