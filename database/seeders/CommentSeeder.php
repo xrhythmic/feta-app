61641454c9cc6a41495cc4d4c83c60c97f9bc28c
Jordan <56432409+xrhythmic@users.noreply.github.com>
2021-11-04 21:26:29 +0000

    Add Manually Seeded Comment and Post CW1

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Comment;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CommentSeeder extends Seeder
@@ -14,6 +17,25 @@ class CommentSeeder extends Seeder
      */
     public function run()
     {
+        // Manually Seeded
+        $user = User::factory()->create();
+
+        Comment::create([
+            'body' => 'A Manually Defined Seeded Post',
+            'post_id' => Post::create([
+                'title' => 'Comment Created Post',
+                'excerpt' => 'A Manually Defined Seeded Post',
+                'body' => 'Body of a Manually Defined Seeded Post',
+                'user_id' => $user->id,
+                'category_id' => Category::create([
+                    'name' => 'Comment Manually Seeded',
+                    'slug' => 'comment-seeded'
+                ])->id
+            ])->id,
+            'user_id' => $user->id
+        ]);
+
+        // Factory Seeded
         Comment::factory(5)->create();
     }
 }
